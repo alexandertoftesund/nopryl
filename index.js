fa3927bb6cdfd2dfdd236d113efb055e387b33ba
Alexander Toftesund <alexandertoftesund@gmail.com>
2025-03-24 10:30:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -11,35 +11,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const randomImage = images[Math.floor(Math.random() * images.length)];
     const homeElement = document.getElementById("home");
+    const gradient = "linear-gradient(var(--menu-color), rgba(255, 255, 255, 0.1))";
 
-    homeElement.style.backgroundImage = `linear-gradient(var(--menu-color), rgba(255, 255, 255, 0.1)), url(${randomImage})`;
+    homeElement.style.backgroundImage = `${gradient}, url(${randomImage})`;
 });
 
-document.querySelectorAll('.scroll-link-top').forEach(link => {
+document.querySelectorAll('.scroll-link, .scroll-link-top').forEach(link => {
     link.addEventListener('click', function(event) {
         event.preventDefault();
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
 
         if (targetElement) {
-            const offsetAboveMiddle = 1000;
-            const targetPosition = targetElement.offsetTop + (targetElement.offsetHeight / 2) - (window.innerHeight / 2) - offsetAboveMiddle;
-            window.scrollTo({
-                top: targetPosition,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
-
-document.querySelectorAll('.scroll-link').forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            const targetPosition = targetElement.offsetTop + (targetElement.offsetHeight / 2) - (window.innerHeight / 2);
+            let targetPosition;
+            if (this.classList.contains('scroll-link-top')) {
+                targetPosition = 0;
+            } else {
+                targetPosition = targetElement.offsetTop + (targetElement.offsetHeight / 2) - (window.innerHeight / 2);
+            }
             window.scrollTo({
                 top: targetPosition,
                 behavior: 'smooth'
